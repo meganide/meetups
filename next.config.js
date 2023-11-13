d943eeb275e8fc97a0ab465288fb3c5783355031
Meganide <renashassan@hotmail.com>
2023-11-13 13:23:59 +0100

    fix: modify next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "meetups",
+  output: "export",
+}
 
 module.exports = nextConfig
