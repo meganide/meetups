b305f35f1e3b0935cfa7cded8a74f96d8cb688f1
Meganide <renashassan@hotmail.com>
2023-11-14 16:09:53 +0100

    fix: add env var

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
 }
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
