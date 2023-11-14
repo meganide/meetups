13e818d3145a2c370d8a404c777574c889d24550
Meganide <renashassan@hotmail.com>
2023-11-14 15:38:24 +0100

    fix: remove comment

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { getServerSession } from "next-auth"
 import authOptions from "@/lib/auth"
 
 export default async function Home() {
-  // const { data: session, status } = useSession()
   const session = await getServerSession(authOptions)
 
   if (!session) {
