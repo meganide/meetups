962bcac417fd84a82fa6de809153f395e375cf27
Meganide <renashassan@hotmail.com>
2023-11-14 14:56:35 +0100

    fix: only add output export on github actions

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+if (isGithubActions) {
+  nextConfig.output = "export"
+}
+
 module.exports = nextConfig
