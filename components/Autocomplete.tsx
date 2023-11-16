9ebdb007c5fee7901fb8dd0a5bc44086e1f1f713
Renas <77303348+meganide@users.noreply.github.com>
2023-11-16 14:43:20 +0100

    feat: add filters (#23)
    * chore: add filter reducer
    
    * feat: filter by date
    
    * chore: refactor filters

--- components/Autocomplete.tsx
+++ components/Autocomplete.tsx
@@ -7,28 +7,28 @@ import TextField from "@mui/material/TextField"
 
 type AutocompleteProps = {
   options: string[]
-  searchQuery: string
-  setSearchQuery: React.Dispatch<React.SetStateAction<string>>
+  inputValue: string
+  onInputChange: (newValue: string) => void
+  label: string
 }
 
 export default function Autocomplete({
   options,
-  searchQuery,
-  setSearchQuery,
+  inputValue,
+  onInputChange,
+  label,
 }: AutocompleteProps) {
   return (
     <Stack spacing={2} sx={{ width: 300 }}>
       <MUIAutocomplete
         id="free-solo"
         freeSolo
-        inputValue={searchQuery}
-        onInputChange={(event: any, newValue: string) => {
-          setSearchQuery(newValue)
-        }}
+        inputValue={inputValue}
+        onInputChange={(event: any, newValue: string) =>
+          onInputChange(newValue)
+        }
         options={options}
-        renderInput={(params) => (
-          <TextField {...params} label="Search meetups" />
-        )}
+        renderInput={(params) => <TextField {...params} label={label} />}
       />
     </Stack>
   )
