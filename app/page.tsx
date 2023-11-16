c6d831dce839f55ef72d41ac85caf7863a60222a
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-11-16 08:04:15 +0100

    feat: styles meetup card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Link from "next/link"
 import useSWR from "swr"
 
 import Meetupcard from "@/components/Meetup/Meetupcard"
@@ -10,14 +9,14 @@ import type { Meetup } from "@prisma/client"
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function Home() {
-  const { data, error } = useSWR("/api/meetups", fetcher)
+  const { data, error, isLoading } = useSWR("/api/meetups", fetcher)
 
   if (error) return <div>Failed to load meetups</div>
-  if (!data) return <div>Loading...</div>
+  if (isLoading) return <div>Loading...</div>
 
   return (
     <section>
-      <h1>Upcoming Meetups</h1>
+      <h1 className="my-5 text-xl font-extrabold">Upcoming Meetups</h1>
       <section className="flex flex-col gap-3">
         {data.meetups.map((meetup: Meetup) => (
           <Meetupcard key={meetup.id} meetup={meetup} />
